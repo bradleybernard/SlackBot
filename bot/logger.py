e66290edf879655f8fcc64aea8b4b4a747f10eb5
Bradley Bernard <bradbernard@me.com>
2019-04-16 10:34:37 -0700

    [Structure] Moved around python files

---
+++ bot/logger.py
@@ -0,0 +1,27 @@
+import os
+import logging.handlers
+import logging
+
+class Logger():
+
+    def __init__(self, log_file, log_level):
+        self.log_file = log_file
+        self.log_level = log_level
+
+        self.setup_logger()
+
+    def setup_logger(self):
+        log_path = os.path.dirname(self.log_file)
+        
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+
+        handler = logging.handlers.WatchedFileHandler(self.log_file)
+        formatter = logging.Formatter(fmt='[%(asctime)s - %(levelname)s]: %(message)s', datefmt='%Y-%m-%d %I:%M:%S %p %z %Z')
+        handler.setFormatter(formatter)
+        
+        logger = logging.getLogger()
+        logger.setLevel(self.log_level)
+        logger.addHandler(handler)
+
+        logging.debug(f'Loging to file: {self.log_file}')
